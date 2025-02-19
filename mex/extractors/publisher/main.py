bec3858cb2207f7bb429351c76718502f3c3ee47
Nicolas Drebenstedt <897972+cutoffthetop@users.noreply.github.com>
2025-02-19 08:30:38 +0100

    feature/mx-1760 implement s3 sink (#331)
    # Added
    - add s3 sink implementation for use with the publisher
    
    # Changes
    - let `load` function accept rule-sets and merged-items, just like sinks
    - let publisher use `load` function with configurable sinks
    
    # Removed
    - remove `PublisherContainer` and `write_merged_items` in favor of
    generic versions
    
    ---------
    
    Co-authored-by: erichesse <hessee@rki.de>

--- mex/extractors/publisher/main.py
+++ mex/extractors/publisher/main.py
@@ -1,24 +1,26 @@
 from mex.common.cli import entrypoint
+from mex.common.models import AnyMergedModel, ItemsContainer
 from mex.extractors.pipeline import asset, run_job_in_process
 from mex.extractors.publisher.extract import get_merged_items
 from mex.extractors.publisher.filter import filter_merged_items
-from mex.extractors.publisher.load import write_merged_items
-from mex.extractors.publisher.models import PublisherContainer
 from mex.extractors.settings import Settings
+from mex.extractors.sinks import load
 
 
 @asset(group_name="publisher")
-def extract_and_filter_merged_items() -> PublisherContainer:
+def extract_and_filter_merged_items() -> ItemsContainer[AnyMergedModel]:
     """Get merged items from mex-backend and filter them by allow-list."""
     items = get_merged_items()
     filtered = list(filter_merged_items(items))
-    return PublisherContainer(items=filtered)
+    return ItemsContainer[AnyMergedModel](items=filtered)
 
 
 @asset(group_name="publisher")
-def publish_merged_items(extract_and_filter_merged_items: PublisherContainer) -> None:
-    """Write received merged items to ndjson file."""
-    write_merged_items(extract_and_filter_merged_items.items)
+def publish_merged_items(
+    extract_and_filter_merged_items: ItemsContainer[AnyMergedModel],
+) -> None:
+    """Write received merged items to configured sink."""
+    load(extract_and_filter_merged_items.items)
 
 
 @entrypoint(Settings)
