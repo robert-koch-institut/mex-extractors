3df9f868b104082853ebf12fd7111024d95151d3
erichesse <hessee@rki.de>
2025-03-13 16:00:17 +0100

    Update mex/extractors/pipeline/wikidata.py
    Co-authored-by: Nicolas Drebenstedt <897972+cutoffthetop@users.noreply.github.com>
    Signed-off-by: erichesse <eric.hesse3@yahoo.de>

--- mex/extractors/pipeline/wikidata.py
+++ mex/extractors/pipeline/wikidata.py
@@ -13,8 +13,8 @@ from mex.extractors.wikidata.helpers import (
 def extracted_organization_rki() -> ExtractedOrganization:
     """Extracts and Transforms RKI organization and loads result."""
     wikidata_organization_ids_by_label = get_wikidata_organization_ids_by_label()
-    rki_id = wikidata_organization_ids_by_label["Robert Koch-Institut"]
-    if extracted_organization_rki := get_wikidata_organization_by_id(rki_id):
-        return extracted_organization_rki
+    if rki_id := wikidata_organization_ids_by_label.get("Robert Koch-Institut"):
+        if extracted_organization_rki := get_wikidata_organization_by_id(rki_id):
+            return extracted_organization_rki
     msg = f"Robert Koch-Institut with ID {rki_id} not found in wikidata"
     raise MExError(msg)
