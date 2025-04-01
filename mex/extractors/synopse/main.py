7170ea2517cababa8bf6cc8a64992832be565218
Eric Heße <hessee@rki.de>
2025-04-01 10:59:18 +0200

    apply all activities filter to synopse

--- mex/extractors/synopse/main.py
+++ mex/extractors/synopse/main.py
@@ -26,6 +26,7 @@ from mex.common.types import (
     MergedPersonIdentifier,
     MergedResourceIdentifier,
 )
+from mex.extractors.filters import filter_by_global_rules
 from mex.extractors.pipeline import asset, run_job_in_process
 from mex.extractors.settings import Settings
 from mex.extractors.sinks import load
@@ -58,9 +59,13 @@ from mex.extractors.utils import load_yaml
 
 
 @asset(group_name="synopse")
-def synopse_projects() -> list[SynopseProject]:
+def synopse_projects(
+    extracted_primary_source_report_server: ExtractedPrimarySource,
+) -> list[SynopseProject]:
     """Extract projects from Synopse."""
-    return list(extract_projects())
+    return filter_by_global_rules(
+        extracted_primary_source_report_server.stableTargetId, extract_projects()
+    )
 
 
 @asset(group_name="synopse")
