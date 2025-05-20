df18a0b8220ba9324e3ecff797f4cc147e2422f2
Eric Heße <hessee@rki.de>
2025-05-20 15:31:56 +0200

    remove deprecated setting

--- mex/extractors/endnote/settings.py
+++ mex/extractors/endnote/settings.py
@@ -7,13 +7,6 @@ from mex.common.types import AssetsPath
 class EndnoteSettings(BaseModel):
     """Settings submodel for the endnote extractor."""
 
-    raw_data_path: AssetsPath = Field(
-        AssetsPath("raw-data/endnote"),
-        description=(
-            "Path to the directory with the endnote xml files, "
-            "absolute path or relative to `assets_dir`."
-        ),
-    )
     mapping_path: AssetsPath = Field(
         AssetsPath("mappings/endnote"),
         description=(
