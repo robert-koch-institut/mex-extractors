dbcfd36a724f31105c5ba0cb55e69e8c081d65a3
Nicolas Drebenstedt <drebenstedtn@rki.de>
2025-05-21 08:58:39 +0200

    sort in-place

--- mex/extractors/sorters.py
+++ mex/extractors/sorters.py
@@ -3,20 +3,22 @@ from typing import TypeVar
 from networkx import DiGraph
 from networkx import topological_sort as network_sort
 
-SortableT = TypeVar("SortableT")
+ItemT = TypeVar("ItemT")
 
 
-def topological_sort(
-    items: list[SortableT], primary_key: str, foreign_key: str
-) -> list[SortableT]:
-    """Sort the given items topologically."""
-    graph = DiGraph()
-    key_to_item: dict[str, SortableT] = {}
+def topological_sort(items: list[ItemT], primary_key: str, foreign_key: str) -> None:
+    """Sort the given list of items in-place according to their topology.
+
+    Items that reference an item's `primary_key` value, in a field specified by
+    `foreign_key`, will be sorted lower in the list as the referenced item.
+
+    This can be useful for submitting items to the backend in the correct order.
+    """
+    graph = DiGraph[str]()
     for item in items:
         pk_value = getattr(item, primary_key)
-        fk_value = getattr(item, foreign_key)
-        key_to_item[pk_value] = item
         graph.add_node(pk_value)
-        if fk_value:
+        if fk_value := getattr(item, foreign_key):
             graph.add_edge(fk_value, pk_value)
-    return [key_to_item[key] for key in network_sort(graph)]
+    sorted_keys = list(network_sort(graph))
+    items.sort(key=lambda item: sorted_keys.index(getattr(item, primary_key)))
