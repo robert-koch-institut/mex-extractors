d14aa0be8c115514b45993a5af56bce469302793
esinsj <72222648+esinsj@users.noreply.github.com>
2025-07-04 17:44:11 +0200

    Update mex/extractors/datenkompass/item.py
    Co-authored-by: erichesse <hessee@rki.de>
    Signed-off-by: esinsj <72222648+esinsj@users.noreply.github.com>

--- mex/extractors/datenkompass/item.py
+++ mex/extractors/datenkompass/item.py
@@ -16,7 +16,7 @@ class DatenkompassActivity(BaseModel):
     datenhalter: str = Field(
         ..., alias="Datenhalter/ Beauftragung durch Behörde im Geschäftsbereich"
     )
-    kontakt: list[str] | None = Field(None, alias="Kontakt (herausgeber)")
+    kontakt: list[str] | None = Field(None, alias="Kontakt (Herausgeber)")
     titel: list[str] | None = Field(None, alias="Titel")
     schlagwort: list[str | None] = Field([], alias="Schlagwort")
     datenbank: list[str] | None = Field(None, alias="Link oder Datenbank")
