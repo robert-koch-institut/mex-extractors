75ed2d0548c05abc478f0a041d8f761567b12157
esinsj <72222648+esinsj@users.noreply.github.com>
2025-07-18 10:27:01 +0200

    feature/mx-1857_publish_datenkompass (#3)
    ### Added
    - publisher for datenkompass
    
    ---------
    
    Signed-off-by: esinsj <72222648+esinsj@users.noreply.github.com>
    Co-authored-by: erichesse <hessee@rki.de>

---
+++ mex/extractors/datenkompass/filter.py
@@ -0,0 +1,30 @@
+from collections.abc import Sequence
+
+from mex.common.logging import logger
+from mex.common.models import AnyMergedModel, MergedActivity
+from mex.extractors.datenkompass.extract import get_merged_items
+
+
+def filter_for_bmg(merged_items: Sequence[AnyMergedModel]) -> list[MergedActivity]:
+    """Filter the merged activities based on the mapping specifications.
+
+    Args:
+        merged_items: list of merged activities as sequence.
+
+    Returns:
+        filtered list of merged activities.
+    """
+    bmg_ids = {
+        bmg.identifier for bmg in get_merged_items("BMG", ["MergedOrganization"], None)
+    }
+
+    bmg_items = [
+        MergedActivity.model_validate(item)
+        for item in merged_items
+        if isinstance(item, MergedActivity)
+        and any(funder in bmg_ids for funder in item.funderOrCommissioner)
+    ]
+
+    logger.info("%s items remain after filtering.", len(bmg_items))
+
+    return bmg_items
