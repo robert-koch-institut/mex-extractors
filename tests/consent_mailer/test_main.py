0241a2dc864c79147b090b311032e291cc8799ca
Nicolas Drebenstedt <drebenstedtn@rki.de>
2025-07-24 17:04:41 +0200

    remove fixture

--- tests/consent_mailer/test_main.py
+++ tests/consent_mailer/test_main.py
@@ -1,8 +1,5 @@
-import pytest
-
 from mex.extractors.pipeline import run_job_in_process
 
 
-@pytest.mark.usefixtures("mocked_backend")
 def test_job() -> None:
     assert run_job_in_process("consent_mailer")
