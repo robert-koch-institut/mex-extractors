8892028e585c617fec67bbcebd6491126e25544a
Eric Heße <hessee@rki.de>
2025-08-05 13:38:04 +0200

    fix igs test main

--- tests/igs/test_main.py
+++ tests/igs/test_main.py
@@ -3,6 +3,6 @@ import pytest
 from mex.extractors.pipeline import run_job_in_process
 
 
-@pytest.mark.usefixtures("mocked_igs", "mocked_ldap")
+@pytest.mark.usefixtures("mocked_igs", "mocked_ldap", "mocked_wikidata")
 def test_job() -> None:
     assert run_job_in_process("igs")
