c363a23e03a75ebe5be29e679e54c9692f78093e
Eric Heße <hessee@rki.de>
2025-08-07 09:18:59 +0200

    variables and groups 1

--- mex/extractors/igs/main.py
+++ mex/extractors/igs/main.py
@@ -9,19 +9,28 @@ from mex.common.models import (
     ExtractedAccessPlatform,
     ExtractedContactPoint,
     ExtractedPrimarySource,
-    ExtractedResource,
     ResourceMapping,
+    VariableGroupMapping,
+    VariableMapping,
 )
 from mex.common.primary_source.transform import get_primary_sources_by_name
-from mex.common.types import MergedOrganizationalUnitIdentifier
+from mex.common.types import (
+    MergedOrganizationalUnitIdentifier,
+    MergedResourceIdentifier,
+    MergedVariableGroupIdentifier,
+)
+
 from mex.extractors.igs.extract import (
     extract_igs_schemas,
     extract_ldap_actors_by_mail,
 )
+from mex.extractors.igs.filter import filter_creation_schemas
 from mex.extractors.igs.model import IGSSchema
 from mex.extractors.igs.transform import (
     transform_igs_access_platform,
     transform_igs_schemas_to_resources,
+    transform_igs_schemas_to_variables,
+    transformed_igs_schemas_to_variable_group,
 )
 from mex.extractors.pipeline.base import run_job_in_process
 from mex.extractors.settings import Settings
@@ -83,21 +92,26 @@ def extracted_igs_contact_points_by_mail(
 
 
 @asset(group_name="igs")
-def extracted_igs_resources(
+def extracted_igs_resource_ids_by_pathogen(
     igs_schemas: dict[str, IGSSchema],
     extracted_primary_source_igs: ExtractedPrimarySource,
     igs_resource_mapping: dict[str, Any],
     extracted_igs_contact_points_by_mail: dict[str, ExtractedContactPoint],
     unit_stable_target_ids_by_synonym: dict[str, MergedOrganizationalUnitIdentifier],
-) -> dict[str, ExtractedResource]:
+) -> dict[str, MergedResourceIdentifier]:
     """Transform IGS resource from IGS schemas."""
-    return transform_igs_schemas_to_resources(
+    extracted_resources = transform_igs_schemas_to_resources(
         igs_schemas,
         extracted_primary_source_igs,
         ResourceMapping.model_validate(igs_resource_mapping),
         extracted_igs_contact_points_by_mail,
         unit_stable_target_ids_by_synonym,
     )
+    load(extracted_resources)
+    return {
+        resource.identifierInPrimarySource.removeprefix("IGS_"): resource.stableTargetId
+        for resource in extracted_resources
+    }
 
 
 @asset(group_name="igs")
@@ -113,6 +127,55 @@ def extracted_igs_access_platform(
         AccessPlatformMapping.model_validate(igs_access_platform_mapping),
         extracted_igs_contact_points_by_mail,
         unit_stable_target_ids_by_synonym,
+    )
+
+
+@asset(group_name="igs")
+def extracted_igs_variable_group_ids_by_identifier_in_primary_source(
+    igs_schemas: dict[str, IGSSchema],
+    extracted_igs_resource_ids_by_pathogen: dict[str, MergedResourceIdentifier],
+    extracted_primary_s"ource_igs: ExtractedPrimarySource,
+) -> dict[str, MergedVariableGroupIdentifier]:
+    """Filter and transform IGS schema to extracted variable group."""
+    settings = Settings.get()
+    variable_group_mapping = VariableGroupMapping.model_validate(
+        load_yaml(settings.igs.mapping_path / "variable-group.yaml")
+    )
+    filtered_schemas = filter_creation_schemas(igs_schemas)
+    extracted_variable_groups = transformed_igs_schemas_to_variable_group(
+        filtered_schemas,
+        variable_group_mapping,
+        extracted_igs_resource_ids_by_pathogen,
+        extracted_primary_source_igs,
+    )
+    load(extracted_variable_groups)
+    return {
+        group.identifierInPrimarySource: group.stableTargetId
+        for group in extracted_variable_groups
+    }
+
+
+@asset(group_name="igs)
+def extracted_igs_variables(
+    igs_schemas: dict[str, IGSSchema],
+    extracted_igs_resource_ids_by_pathogen: dict[str, MergedResourceIdentifier],
+    extracted_primary_source_igs: ExtractedPrimarySource,
+    extracted_igs_variable_group_ids_by_identifier_in_primary_source: dict[
+        str, MergedVariableGroupIdentifier
+    ],
+) -> None:
+    settings = Settings.get()
+    variable_mapping = VariableMapping.model_validate(
+        load_yaml(settings.igs.mapping_path / "access-platform.yaml")
+    )
+    load(
+        transform_igs_schemas_to_variables(
+            igs_schemas,
+            extracted_igs_resource_ids_by_pathogen,
+            extracted_primary_source_igs,
+            extracted_igs_variable_group_ids_by_identifier_in_primary_source,
+            variable_mapping,
+        )
     )
 
 
