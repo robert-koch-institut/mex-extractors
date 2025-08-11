bccdd5ae27673d25740db8a5b9c7fe7c7da10d5a
erichesse <hessee@rki.de>
2025-08-11 08:31:03 +0200

    Feature/mx 1910 update synopse according to new mapping (#44)
    ### PR Context
    
    - assets PR 257
    
    ### Changes
    
    - update synopse according to mapping changes
    
    ---------
    
    Signed-off-by: erichesse <eric.hesse3@yahoo.de>
    Co-authored-by: esinsj <72222648+esinsj@users.noreply.github.com>

--- tests/synopse/test_filter.py
+++ tests/synopse/test_filter.py
@@ -1,29 +1,16 @@
 from mex.common.models import ExtractedPrimarySource
 from mex.extractors.synopse.filter import (
-    filter_and_log_access_platforms,
     filter_and_log_synopse_variables,
 )
-from mex.extractors.synopse.models.study import SynopseStudy
 from mex.extractors.synopse.models.variable import SynopseVariable
-
-
-def test_filter_and_log_access_platforms(
-    synopse_studies: list[SynopseStudy],
-    extracted_primary_sources: dict[str, ExtractedPrimarySource],
-) -> None:
-    assert len(list(synopse_studies)) == 6
-    synopse_studies_filtered = filter_and_log_access_platforms(
-        synopse_studies, extracted_primary_sources["report-server"]
-    )
-    assert len(list(synopse_studies_filtered)) == 5
 
 
 def test_filter_and_log_synopse_variables(
     synopse_variables: list[SynopseVariable],
     extracted_primary_sources: dict[str, ExtractedPrimarySource],
 ) -> None:
-    assert len(list(synopse_variables)) == 6
+    assert len(list(synopse_variables)) == 3
     synopse_variables_filtered = filter_and_log_synopse_variables(
         synopse_variables, extracted_primary_sources["report-server"]
     )
-    assert len(list(synopse_variables_filtered)) == 4
+    assert len(list(synopse_variables_filtered)) == 1
