bed2799f3a593ea7de38ad0b774013ad4779ebeb
Fabian Eckelmann <EckelmannF@rki.de>
2025-08-22 11:31:51 +0200

    using mailpit now

--- mex/extractors/consent_mailer/settings.py
+++ mex/extractors/consent_mailer/settings.py
@@ -1,4 +1,4 @@
-from pydantic import Field
+from pydantic import Field, SecretStr
 
 from mex.common.settings import BaseSettings
 from mex.common.types import AssetsPath
@@ -7,10 +7,28 @@ from mex.common.types import AssetsPath
 class ConsentMailerSettings(BaseSettings):
     """Settings definition class for the consent mailer."""
 
+    mailpit_api_url: str = Field(
+        "localhost:8025",
+        description="The url to the api endpoint for mailpit. USED FOR TESTS ONLY!",
+    )
+    mailpit_api_user: SecretStr = Field(
+        SecretStr("user"),
+        description="The username used for Basic HTTP auth against the mailpit api."
+        " USED FOR TESTS ONLY!",
+    )
+    mailpit_api_password: SecretStr = Field(
+        SecretStr("password"),
+        description="The password used for Basic HTTP auth against the mailpit api."
+        "USED FOR TESTS ONLY!",
+    )
     schedule: str = Field(
         "0 0 * * *",
         description="A valid cron string defining when to run the consent mailer",
-        validation_alias="MEX_CONSENT_MAILER_SCHEDULE",
+    )
+    smtp_server: str = Field(
+        "localhost:1025",
+        description="Address and port (<address>:<port>) of the SMTP server to use for"
+        " the consent mailer.",
     )
     template_path: AssetsPath = Field(
         AssetsPath("mailings"),
@@ -18,9 +36,4 @@ class ConsentMailerSettings(BaseSettings):
             "Path to the directory with the jinja template file containing the"
             " email body template, absolute path or relative to `assets_dir`."
         ),
-    )
-    smtp_server: str = Field(
-        "localhost:1025",
-        description="Address and port (<address>:<port>) of the SMTP server to use for"
-        " the consent mailer.",
     )
--- tests/consent_mailer/test_main.py
+++ tests/consent_mailer/test_main.py
@@ -1,13 +1,16 @@
+import base64
+from typing import Any
+
 import pytest
 import requests
 
 from mex.common.models import MergedPerson
+from mex.common.types import AssetsPath
 from mex.extractors.consent_mailer.main import (
     send_consent_emails_to_persons,
 )
 from mex.extractors.pipeline import run_job_in_process
-
-MAIL_HOG_API_URL = "http://localhost:8025"
+from mex.extractors.settings import Settings
 
 
 @pytest.mark.usefixtures("mocked_consent_backend_api_connector")
@@ -15,10 +18,46 @@ def test_job() -> None:
     assert run_job_in_process("consent_mailer")
 
 
+def _req_header() -> dict[str, str]:
+    settings = Settings.get()
+    return {
+        "Authorization": "Basic "
+        + base64.b64encode(
+            f"{settings.consent_mailer.mailpit_api_user.get_secret_value()}:{settings.consent_mailer.mailpit_api_password.get_secret_value()}".encode()
+        ).decode()
+    }
+
+
+def _req_verify() -> bool | str:
+    settings = Settings.get()
+    if isinstance(settings.verify_session, AssetsPath):
+        return settings.verify_session._path.absolute().as_posix()
+    return settings.verify_session
+
+
+def _delete_messages() -> None:
+    settings = Settings.get()
+    requests.delete(
+        f"{settings.consent_mailer.mailpit_api_url}/api/v1/messages",
+        timeout=3,
+        verify=_req_verify(),
+        headers=_req_header(),
+    )
+
+
+def _get_messages() -> Any:  # noqa: ANN401
+    settings = Settings.get()
+    return requests.get(
+        f"{settings.consent_mailer.mailpit_api_url}/api/v1/messages",
+        timeout=3,
+        verify=_req_verify(),
+        headers=_req_header() | {"accept": "application/json"},
+    ).json()
+
+
 @pytest.mark.integration  # disabled on gh cli due to no mailHog available
 def test_send_consent_emails_to_persons() -> None:
-    requests.delete(f"{MAIL_HOG_API_URL}/api/v1/messages", timeout=3)
-
+    _delete_messages()
     persons = [
         MergedPerson(
             identifier="efVXbcoxxMAiA5IqT1d11H",
@@ -39,17 +78,17 @@ def test_send_consent_emails_to_persons() -> None:
 
     send_consent_emails_to_persons(persons)
 
-    envelop = requests.get(f"{MAIL_HOG_API_URL}/api/v2/messages", timeout=3).json()
+    envelop = _get_messages()
     assert envelop["total"] == 1
     assert all(
-        all(x["Domain"] == "rki.de" for x in message["To"])
-        for message in envelop["items"]
+        all(x["Address"].endswith("rki.de") for x in message["To"])
+        for message in envelop["messages"]
     )
 
 
 @pytest.mark.integration  # disabled on gh cli due to no mailHog available
 def test_send_consent_no_emails_for_no_rki_persons() -> None:
-    requests.delete(f"{MAIL_HOG_API_URL}/api/v1/messages", timeout=3)
+    _delete_messages()
 
     persons = [
         MergedPerson(
@@ -71,5 +110,5 @@ def test_send_consent_no_emails_for_no_rki_persons() -> None:
 
     send_consent_emails_to_persons(persons)
 
-    messages = requests.get(f"{MAIL_HOG_API_URL}/api/v2/messages", timeout=3).json()
-    assert messages["total"] == 0
+    envelop = _get_messages()
+    assert envelop["total"] == 0
