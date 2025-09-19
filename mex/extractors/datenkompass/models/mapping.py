9e7207132433803187d752b74d2a99b7d07d05f7
Janina Esins <esinsj@rki.de>
2025-09-19 13:34:28 +0200

    set up mapping model

---
+++ mex/extractors/datenkompass/models/mapping.py
@@ -0,0 +1,33 @@
+from typing import Annotated
+
+from pydantic import BaseModel, Field
+
+from mex.common.models.base.mapping import BaseMapping
+
+
+class MappingRule(BaseModel, extra="forbid"):
+    """Generic mapping rule model."""
+
+    forValues: Annotated[list[str] | None, Field(title="forValues")] = None
+    setValues: Annotated[str | None, Field(title="setValues")] = None
+    rule: Annotated[str | None, Field(title="rule")] = None
+    expectedOutputExample: Annotated[
+        list[str] | None, Field(title="expectedOutputExample")
+    ] = None
+
+
+class DatenkompassMappingField(BaseMapping, extra="forbid"):
+    """Model subclass for each datenkompass field mapping."""
+
+    fieldInTarget: Annotated[str | None, Field(title="fieldInTarget")] = None
+    fieldInMEx: Annotated[str | None, Field(title="fieldInMEx")] = None
+    mappingRules: Annotated[
+        list[MappingRule], Field(min_length=1, title="mappingRules")
+    ]
+    comment: Annotated[str | None, Field(title="fieldInTarget")] = None
+
+
+class DatenkompassMapping(BaseMapping, extra="forbid"):
+    """A mapping for the Datenkompass mapping yamls."""
+
+    fields: list[DatenkompassMappingField]
