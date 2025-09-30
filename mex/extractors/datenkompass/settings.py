e8095d507b76af711ef5f8aad1d7f94a317238da
esinsj <72222648+esinsj@users.noreply.github.com>
2025-09-30 11:00:57 +0200

    Feature/mx 2008 read default from mapping (#73)
    ### Changes
    - read datenkompass default values from mapping

--- mex/extractors/datenkompass/settings.py
+++ mex/extractors/datenkompass/settings.py
@@ -1,6 +1,7 @@
 from pydantic import Field
 
 from mex.common.models import BaseModel
+from mex.common.types import AssetsPath
 
 
 class DatenkompassSettings(BaseModel, str_strip_whitespace=False):
@@ -17,4 +18,11 @@ class DatenkompassSettings(BaseModel, str_strip_whitespace=False):
     list_delimiter: str = Field(
         "; ",
         description="Seperator for different entries in a datenkompass model field.",
+    )
+    mapping_path: AssetsPath = Field(
+        AssetsPath("mappings/mapping-to-external-schema/datenkompass"),
+        description=(
+            "Path to the directory with the datenkompass mapping files containing the "
+            "default values, absolute path or relative to `assets_dir`."
+        ),
     )
