4f1aa31a5437a0049b83dd80f36b601bb849a292
rababerladuseladim <rababerladuseladim@users.noreply.github.com>
2025-10-02 15:01:37 +0200

    Feature/mx2005 fix list field order (#82)
    ### PR Context
    
    To find the elements with random list order, I did the following:
    
    * compare the published json files on prod for two consecutive days
    (2025-09-18 and 2025-09-19)
    * For all items present in both files
      * check if their list attributes have the same set of elements. 
    * If they have the same set of elements, compare the order of list
    elements
    * if they differ in order: report item identifier and attribute that
    differs in list element order
    * for each differing item
    * get the identifierInPrimarySource of the source ExtractedPrimarySource
    and entity type from neo4j prod
    * for each set of (primary source, entity type, attribute), check code
    for source of random list order
    
    ### Fixed
    
    - random list element order in some extracted entity attributes

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -23,6 +23,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Fixed
 
 - odk variable identifier in primary source
+- random list element order in some extracted entity attributes
 
 ### Security
 
--- mex/extractors/open_data/transform.py
+++ mex/extractors/open_data/transform.py
@@ -49,7 +49,7 @@ def transform_open_data_person_affiliations_to_organizations(
         list of Extracted Organization Ids by affiliation name
     """
     unique_affiliations = {
-        person.affiliation
+        person.affiliation: None
         for person in extracted_open_data_creators_contributors
         if person.affiliation
     }
@@ -286,7 +286,7 @@ def transform_open_data_parent_resource_to_mex_resource(  # noqa: PLR0913
     for resource in open_data_parent_resource:
         contributing_unit = list(
             {
-                unit_id
+                unit_id: None
                 for person in resource.metadata.contributors
                 + resource.metadata.creators
                 if (
