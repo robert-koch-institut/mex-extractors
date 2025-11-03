a773f5ca6a0c8af66d8cb9bde4a4e4dfe5c621a8
Nicolas Drebenstedt <drebenstedtn@rki.de>
2025-11-03 15:52:07 +0100

    lint

--- mex/extractors/organigram/helpers.py
+++ mex/extractors/organigram/helpers.py
@@ -1,5 +1,6 @@
 from functools import lru_cache
 
+from mex.common.exceptions import EmptySearchResultError
 from mex.common.organigram.extract import (
     extract_organigram_units,
     get_extracted_unit_by_synonyms,
@@ -12,7 +13,9 @@ from mex.extractors.primary_source.helpers import (
     get_extracted_primary_source_id_by_name,
 )
 from mex.extractors.sinks import load
-from mex.extractors.wikidata.helpers import get_wikidata_organization_ids_by_label
+from mex.extractors.wikidata.helpers import (
+    get_wikidata_organization_by_id,
+)
 
 
 @lru_cache(maxsize=1024)
@@ -28,16 +31,21 @@ def get_unit_merged_id_by_synonym(
     Returns:
         merged organizational unit id if found else None
     """
-    wikidata_organization_ids_by_label = get_wikidata_organization_ids_by_label()
+    rki_organization = get_wikidata_organization_by_id("RKI")
+    if not rki_organization:
+        msg = "RKI wikidata organization not found"
+        raise EmptySearchResultError(msg)
     organigram_units = extract_organigram_units()
     mex_organizational_units = transform_organigram_units_to_organizational_units(
         organigram_units,
         get_extracted_primary_source_id_by_name("organigram"),
-        wikidata_organization_ids_by_label("RKI"),
+        rki_organization,
     )
-    unit_merged_ids_by_synonyms = get_extracted_unit_by_synonyms(organigram_units)
+    unit_merged_ids_by_synonyms = get_extracted_unit_by_synonyms(
+        mex_organizational_units
+    )
     if synonym not in unit_merged_ids_by_synonyms:
         return None
     mex_organizational_unit = unit_merged_ids_by_synonyms[synonym]
     load([mex_organizational_unit])
-    return mex_organizational_unit
+    return mex_organizational_unit.stableTargetId
