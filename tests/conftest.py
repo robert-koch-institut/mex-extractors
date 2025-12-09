31c793e3bc58f37fd7ef7aaaa3648b7713515fcc
Janina Esins <esinsj@rki.de>
2025-12-09 18:17:39 +0100

    empty loaded merged unit id cache before each test

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,7 @@ import pytest
 
 from mex.common.models import ExtractedOrganization
 from mex.common.types import MergedPrimarySourceIdentifier
+from mex.extractors.organigram.helpers import _get_cached_unit_merged_ids_by_synonyms
 from mex.extractors.settings import Settings
 
 pytest_plugins = (
@@ -27,6 +28,12 @@ TEST_DATA_DIR = Path(__file__).parent / "test_data"
 def settings() -> Settings:
     """Load the settings for this pytest session."""
     return Settings.get()
+
+
+@pytest.fixture(autouse=True)
+def isolate_unit_cache() -> None:
+    # clear the cache to be able to test it.
+    _get_cached_unit_merged_ids_by_synonyms.cache_clear()
 
 
 @pytest.fixture
