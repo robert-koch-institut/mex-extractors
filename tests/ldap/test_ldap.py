511549d632db9e8fd7eb8628947722bf900104ad
Janina Esins <esinsj@rki.de>
2025-12-11 12:02:42 +0100

    adapt looking into

--- tests/ldap/test_ldap.py
+++ tests/ldap/test_ldap.py
@@ -7,7 +7,7 @@ from mex.extractors.settings import Settings
 
 @pytest.mark.integration
 def test_mock_available() -> None:
-    ldap_connector = LDAPConnector.get()
     print(Settings.get().ldap_url.get_secret_value())
+    ldap_connector = LDAPConnector.get()
     assert ldap_connector.get_persons(limit=42)
     assert not ldap_connector.get_persons(limit=42)
