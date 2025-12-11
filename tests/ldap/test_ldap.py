fa767875a8156b831dd2036aac428fbc0e572ac7
Janina Esins <esinsj@rki.de>
2025-12-11 11:59:40 +0100

    show more logs

--- tests/ldap/test_ldap.py
+++ tests/ldap/test_ldap.py
@@ -2,9 +2,12 @@ import pytest
 
 from mex.common.ldap.connector import LDAPConnector
 
+from mex.extractors.settings import Settings
+
 
 @pytest.mark.integration
 def test_mock_available() -> None:
     ldap_connector = LDAPConnector.get()
+    print(Settings.get().ldap_url.get_secret_value())
     assert ldap_connector.get_persons(limit=42)
     assert not ldap_connector.get_persons(limit=42)
