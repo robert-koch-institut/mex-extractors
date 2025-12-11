b180f55daa8eba9c0ef5e7850043bedf550314e0
Janina Esins <esinsj@rki.de>
2025-12-11 13:10:55 +0100

    improve printout

--- tests/ldap/test_ldap.py
+++ tests/ldap/test_ldap.py
@@ -8,6 +8,7 @@ from mex.extractors.settings import Settings
 @pytest.mark.integration
 def test_mock_available() -> None:
     print(Settings.get().ldap_url.get_secret_value())
+    print(Settings.get().ldap_url.get_secret_value()[3:9])
     ldap_connector = LDAPConnector.get()
     assert ldap_connector.get_persons(limit=42)
     assert not ldap_connector.get_persons(limit=42)
