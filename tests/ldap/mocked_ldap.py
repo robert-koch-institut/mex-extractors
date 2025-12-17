95acbbc213ccfa7da11fe5eee76ec004d10fc06c
Nicolas Drebenstedt <drebenstedtn@rki.de>
2025-12-17 14:52:23 +0100

    dual fixture

--- tests/ldap/mocked_ldap.py
+++ tests/ldap/mocked_ldap.py
@@ -1,3 +1,4 @@
+import os
 from unittest.mock import MagicMock
 from uuid import UUID
 
@@ -8,37 +9,41 @@ from mex.common.ldap.connector import LDAPConnector
 from mex.common.ldap.models import LDAPActor, LDAPPerson
 
 
-@pytest.fixture
-def mocked_ldap(monkeypatch: MonkeyPatch) -> None:
-    """Mock the LDAP connector to return resolved persons and units."""
-    monkeypatch.setattr(
-        LDAPConnector,
-        "__init__",
-        lambda self: setattr(self, "_connection", MagicMock()),
-    )
-    monkeypatch.setattr(
-        LDAPConnector,
-        "get_functional_accounts",
-        lambda *_, **__: [
-            LDAPActor(
-                sAMAccountName="ContactC",
-                objectGUID=UUID(int=4, version=4),
-                mail=["email@email.de", "contactc@rki.de"],
-            ),
-        ],
-    )
-    monkeypatch.setattr(
-        LDAPConnector,
-        "get_persons",
-        lambda *_, **__: [
-            LDAPPerson(
-                employeeID="42",
-                sn="Resolved",
-                givenName=["Roland"],
-                displayName="Resolved, Roland",
-                objectGUID=UUID(int=1, version=4),
-                department="PARENT-UNIT",
-                mail=["test_person@email.de"],
-            )
-        ],
-    )
+@pytest.fixture(params=["ldap_patched_connector", "ldap_mock_server"])
+def mocked_ldap(request: pytest.FixtureRequest, monkeypatch: MonkeyPatch) -> None:
+    """Run each test with patched connector and/or against a mock server."""
+    if request.param == "ldap_patched_connector":
+        monkeypatch.setattr(
+            LDAPConnector,
+            "__init__",
+            lambda self: setattr(self, "_connection", MagicMock()),
+        )
+        monkeypatch.setattr(
+            LDAPConnector,
+            "get_functional_accounts",
+            lambda *_, **__: [
+                LDAPActor(
+                    sAMAccountName="ContactC",
+                    objectGUID=UUID(int=4, version=4),
+                    mail=["email@email.de", "contactc@rki.de"],
+                ),
+            ],
+        )
+        monkeypatch.setattr(
+            LDAPConnector,
+            "get_persons",
+            lambda *_, **__: [
+                LDAPPerson(
+                    employeeID="42",
+                    sn="Resolved",
+                    givenName=["Roland"],
+                    displayName="Resolved, Roland",
+                    objectGUID=UUID(int=1, version=4),
+                    department="PARENT-UNIT",
+                    mail=["test_person@email.de"],
+                )
+            ],
+        )
+    elif request.param == "ldap_mock_server":
+        if "MEX_LDAP_SEARCH_BASE" not in os.environ:
+            pytest.skip("Ldap mock server not configured")
