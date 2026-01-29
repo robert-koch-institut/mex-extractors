561ddb3dc999b20a657d6d1a714d44f4ca51d346
Janina Esins <esinsj@rki.de>
2026-01-29 10:20:30 +0100

    update breaking changes from mex-artificial

--- mex/extractors/artificial/main.py
+++ mex/extractors/artificial/main.py
@@ -1,7 +1,7 @@
 from dagster import asset
 
 from mex.artificial.constants import DEFAULT_LOCALE
-from mex.artificial.helpers import generate_artificial_extracted_items
+from mex.artificial.helpers import create_artificial_extracted_items
 from mex.common.cli import entrypoint
 from mex.common.models import (
     EXTRACTED_MODEL_CLASSES_BY_NAME,
@@ -16,11 +16,12 @@ from mex.extractors.sinks import load
 @asset(group_name="artificial")
 def artificial_data() -> ItemsContainer[AnyExtractedModel]:
     """Load the artificial data models to the sinks."""
-    artificial_data = generate_artificial_extracted_items(
+    artificial_data = create_artificial_extracted_items(
         locale=DEFAULT_LOCALE,
         seed=42,
         chattiness=16,
         stem_types=list(EXTRACTED_MODEL_CLASSES_BY_NAME),
+        count=len(EXTRACTED_MODEL_CLASSES_BY_NAME) * 25,
     )
     load(artificial_data)
     return ItemsContainer[AnyExtractedModel](items=artificial_data)
