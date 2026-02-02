7cfc0b70dcab5c24c06cb80593f3e3568f26bfa2
Eric Heße <hessee@rki.de>
2026-02-02 10:42:19 +0100

    remove test

--- tests/ldap/test_helpers.py
+++ tests/ldap/test_helpers.py
@@ -1,12 +1,10 @@
 import pytest
 
 from mex.common.types import (
-    MergedContactPointIdentifier,
     MergedOrganizationalUnitIdentifier,
     MergedPersonIdentifier,
 )
 from mex.extractors.ldap.helpers import (
-    get_ldap_merged_contact_id_by_mail,
     get_ldap_merged_person_id_by_query,
 )
 
@@ -20,14 +18,3 @@ def test_get_ldap_merged_person_id_by_query(
         display_name="Resolved, Roland",
     )
     assert merged_person_id == MergedPersonIdentifier("eXA2Qj5pKmI7HXIgcVqCfz")
-
-
-@pytest.mark.integration
-def test_get_ldap_merged_contact_id_by_mail() -> None:
-    merged_contact_point_id = get_ldap_merged_contact_id_by_mail(
-        mail="resolvedr@rki.de"
-    )
-
-    assert merged_contact_point_id == MergedContactPointIdentifier(
-        "cMkmnNOoNVAohBA1XLNr9K"
-    )
