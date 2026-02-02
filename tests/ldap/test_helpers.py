2316fc855f23400cfd037941369919315109f855
Eric Heße <hessee@rki.de>
2026-02-02 11:58:39 +0100

    fix local test

--- tests/ldap/test_helpers.py
+++ tests/ldap/test_helpers.py
@@ -13,6 +13,7 @@ from mex.extractors.ldap.helpers import (
 
 @pytest.mark.integration
 @pytest.mark.requires_rki_infrastructure
+@pytest.mark.usefixtures("mocked_ldap")
 def test_get_ldap_merged_person_id_by_query(
     mocked_merged_organizational_unit_ids: list[MergedOrganizationalUnitIdentifier],
 ) -> None:
@@ -25,6 +26,7 @@ def test_get_ldap_merged_person_id_by_query(
 
 @pytest.mark.integration
 @pytest.mark.requires_rki_infrastructure
+@pytest.mark.usefixtures("mocked_ldap")
 def test_get_ldap_merged_contact_id_by_mail() -> None:
     merged_contact_point_id = get_ldap_merged_contact_id_by_mail(
         mail="resolvedr@rki.de"
