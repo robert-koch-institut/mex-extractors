c77aff5a2ca6bdd2f2af416deba2b6a779134c2b
Eric Heße <hessee@rki.de>
2026-02-02 10:24:42 +0100

    fix tests

--- tests/ldap/test_helpers.py
+++ tests/ldap/test_helpers.py
@@ -11,8 +11,8 @@ from mex.extractors.ldap.helpers import (
 )
 
 
-@pytest.mark.integration
 @pytest.mark.usefixtures("mocked_wikidata")
+@pytest.mark.integration
 def test_get_ldap_merged_person_id_by_query(
     mocked_merged_organizational_unit_ids: list[MergedOrganizationalUnitIdentifier],
 ) -> None:
@@ -23,8 +23,8 @@ def test_get_ldap_merged_person_id_by_query(
     assert merged_person_id == MergedPersonIdentifier("eXA2Qj5pKmI7HXIgcVqCfz")
 
 
-@pytest.mark.integration
 @pytest.mark.usefixtures("mocked_wikidata")
+@pytest.mark.integration
 def test_get_ldap_merged_contact_id_by_mail() -> None:
     merged_contact_point_id = get_ldap_merged_contact_id_by_mail(
         mail="resolvedr@rki.de"
