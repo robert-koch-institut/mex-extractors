9e73f290132f336003fe5c5f27baa95f9dea2827
Janina Esins <esinsj@rki.de>
2026-06-17 14:55:35 +0200

    update Sumo

--- mex/extractors/sumo/transform.py
+++ mex/extractors/sumo/transform.py
@@ -128,9 +128,9 @@ def transform_resource_feat_model_to_mex_resource(
         .mappingRules[0]
         .setValues[0]  # type: ignore[index]
         .value,
-        isPartOf=mex_resource_nokeda.stableTargetId,
         keyword=keyword,
         meshId=sumo_resource_feat.meshId[0].mappingRules[0].setValues[0],  # type: ignore[index]
+        relatedResource=mex_resource_nokeda.stableTargetId,
         resourceCreationMethod=sumo_resource_feat.resourceCreationMethod[0]
         .mappingRules[0]
         .setValues[0],  # type: ignore[index]
