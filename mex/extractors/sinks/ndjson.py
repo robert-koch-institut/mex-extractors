238dbc9ae7ce85a276d511750fad6bbbd2fe7044
vyvytranngoc <tran-ngocv@rki.de>
2026-08-20 11:16:55 +0200

    Feature/mx 1808 publisher config sink (#313)
    ### PR Context
    Devs need to debug the publisher by looking at the published data. To
    make this easier, we want to add the config option to publish to ndjson
    
    ### Added
    - extrators settings has configuration option for publishing sink:
    extactor_settings.publisher.sink
    - allowed values for publishing sink: ndjson or S3 (default: S3)
    
    ### Changes
    - publisher is not hardcoded anymore to s3
    
    ---------
    
    Signed-off-by: vyvytranngoc <tran-ngocv@rki.de>

---
+++ mex/extractors/sinks/ndjson.py
@@ -0,0 +1,27 @@
+import json
+from pathlib import Path
+from typing import TYPE_CHECKING, TypeVar
+
+from mex.common.models import BaseModel
+from mex.common.sinks.base import BaseSink
+from mex.common.transform import MExEncoder
+from mex.extractors.settings import ExtractorsSettings
+
+if TYPE_CHECKING:
+    from collections.abc import Generator, Iterable
+
+_LoadItemT = TypeVar("_LoadItemT", bound=BaseModel)
+
+
+class NdjsonSink(BaseSink):
+    """Sink that writes models as NDJSON to a local file."""
+
+    def load(self, items: Iterable[_LoadItemT]) -> Generator[_LoadItemT]:
+        """Write items as NDJSON to a local file."""
+        settings = ExtractorsSettings.get()
+        output_path = Path(settings.work_dir) / "publisher_items.ndjson"
+        with output_path.open("w", encoding="utf-8") as f:
+            for item in items:
+                f.write(json.dumps(item, sort_keys=True, cls=MExEncoder))
+                f.write("\n")
+                yield item
