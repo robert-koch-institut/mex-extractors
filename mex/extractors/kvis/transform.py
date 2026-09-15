ab36fa4ae8364d8a9d2fdc7d534bb7fc88f460d8
erichesse <hessee@rki.de>
2026-09-15 17:00:28 +0200

    Feature/mx 2373 update kvis (#411)
    ### PR Context
    - kvis mapping update

--- mex/extractors/kvis/transform.py
+++ mex/extractors/kvis/transform.py
@@ -106,6 +106,7 @@ def transform_kvis_resource_to_extracted_resource() -> ExtractedResource:
         documentation=mapping.documentation[0].mappingRules[0].setValues,
         externalPartner=external_partner,
         hadPrimarySource=get_extracted_primary_source_id_by_name("kvis"),
+        hasCodingSystem=mapping.hasCodingSystem[0].mappingRules[0].setValues,
         hasLegalBasis=mapping.hasLegalBasis[0].mappingRules[0].setValues,
         hasPurpose=mapping.hasPurpose[0].mappingRules[0].setValues,
         healthCategory=mapping.healthCategory[0].mappingRules[0].setValues,
